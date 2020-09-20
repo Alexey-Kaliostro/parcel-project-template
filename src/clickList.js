eaf73ae59601b35fb2e4b1c4df86832f7f4b4abf
Stas-Ukrop <48555868+Stas-Ukrop@users.noreply.github.com>
2020-09-20 11:17:50 +0300

    add js for slaider

--- src/clickList.js
+++ src/clickList.js
@@ -2,7 +2,7 @@
     const refs = {
         leftBtn: document.querySelector('[data-modal-left]'),
         rightBtn: document.querySelector('[data-modal-right]'),
-        mainFoto:document.querySelector('data-img-change'),
+        mainFoto: document.querySelector('[data-img-change]'),
         modal: document.querySelectorAll('[data-img]'),
     };
   let n=0;
@@ -12,24 +12,30 @@
     function leftClick() {
         n-=1;
       if(0<=n && n<refs.modal.length){
-        refs.modal[n].src;
-        console.log(refs.modal[n].src);
+        refs.mainFoto.src=refs.modal[n].src;
+        addClass(n);
       }else{
           n=refs.modal.length-1;
-          refs.modal[n].src;
-        console.log(refs.modal[n].src);
-
+          refs.mainFoto.src=refs.modal[n].src;
+          addClass(n);
       }
     }
     function rightClick() {
         n+=1;
         if(0<=n && n<refs.modal.length){
-            refs.modal[n].src;
-            console.log(refs.modal[n].src);
+            refs.mainFoto.src=refs.modal[n].src;
+            addClass(n);
         }else{
             n=0;
-            refs.modal[n].src;
-        console.log(refs.modal[n].src);
+            refs.mainFoto.src=refs.modal[n].src;
+            addClass(n);
         }
-      }
+    }
+    function addClass(n){
+        for(let i=0;i<refs.modal.length;i++){
+            refs.modal[i].classList.add('my-border-white');
+            refs.modal[i].classList.remove('my-border');
+        }
+        refs.modal[n].classList.add('my-border');
+    }
   })();
